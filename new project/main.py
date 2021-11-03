15ee383f4dfe3db804e8049944d131a38941aecd
mahurekunal <93585092+mahurekunal@users.noreply.github.com>
2021-11-03 13:20:20 +0530

    Add files via upload
    project1

---
+++ new project/main.py
@@ -0,0 +1,34 @@
+import phonenumbers
+import opencage
+
+from myphone import number
+
+from phonenumbers import geocoder
+
+pepnumber=phonenumbers.parse(number)
+location=geocoder.description_for_number(pepnumber, "en")
+print(location)
+
+from phonenumbers import carrier
+service_pro=phonenumbers.parse(number)
+print(carrier.name_for_number(service_pro, "en"))
+
+from opencage.geocoder import OpenCageGeocode
+
+key='bc2db3b22eb34911bee5d452426649d3'
+
+geocoder = OpenCageGeocode(key)
+query=str(location)
+results=geocoder.geocode(query)
+#print(results)
+
+lat=results[0]['geometry']['lat']
+lng=results[0]['geometry']['lng']
+
+print(lat,lng)
+
+
+
+
+
+
